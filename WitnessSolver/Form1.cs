6bc1cc5aa283997f4c449f0decdeaa95e932f9aa
Hilton Lange <hilange@microsoft.com>
2017-03-17 16:59:42 -0700

    New puzzle 1

--- WitnessSolver/Form1.cs
+++ WitnessSolver/Form1.cs
@@ -204,6 +204,22 @@ namespace WitnessSolver
             return puzzle;
         }
 
+        private Puzzle NewPuzzle()
+        {
+            RectanglePuzzle puzzle = new RectanglePuzzle(3, 3, new Point(3, 0), new Point(0, 1));
+            puzzle.Points[0, 2].MustTraverse = true;
+            puzzle.Points[1, 2].MustTraverse = true;
+            puzzle.Points[2, 1].MustTraverse = true;
+            puzzle.Points[3, 1].MustTraverse = true;
+
+            puzzle.Points[0, 0].OutEdges[puzzle.Points[1, 0]].MayTraverse = false;
+            puzzle.Points[0, 0].OutEdges[puzzle.Points[0, 1]].MayTraverse = false;
+            puzzle.Points[2, 1].OutEdges[puzzle.Points[3, 1]].MayTraverse = false;
+            puzzle.Points[2, 2].OutEdges[puzzle.Points[2, 3]].MayTraverse = false;
+
+            return puzzle;
+        }
+
         private Puzzle MiddleChurch()
         {
             RectanglePuzzle puzzle = new RectanglePuzzle(4, 4, new Point(0, 4), new Point(4, 0));
